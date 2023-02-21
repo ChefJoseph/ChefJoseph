40fc76ae2bf60aa2c0209b79a5f90e137e3f5101
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-02-21 00:41:26 -0500

    Time: 251 ms, Memory: 57.2 MB - LeetHub

--- sort-an-array/sort-an-array.js
+++ sort-an-array/sort-an-array.js
@@ -2,6 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// The heapSort function uses two helper functions: heapify and swap.
+
+// The heapify function takes an array, an index, and a heap size. It compares the value at the current index with its left and right children (if they exist) to find the largest value. If the largest value is not at the current index, it swaps the values and recursively calls heapify on the new index.
+
+// The swap function simply swaps two elements in the array.
+
+// The heapSort function first heapifies the input array to satisfy the max-heap property. Then, it repeatedly extracts the maximum element from the heap (which is always at the root) and places it at the end of the array. After all elements have been extracted, the array is sorted in ascending order.
+
+// The time complexity of heap sort is O(n log n), which is achieved by the heapify operation that takes O(log n) time for each node in the heap. The space complexity is O(1), as the sort is performed in-place.
 var sortArray = function(nums) {
       // Heapify the array to satisfy the max-heap property
   for (let i = Math.floor(nums.length / 2) - 1; i >= 0; i--) {
